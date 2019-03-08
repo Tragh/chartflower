03dd6db2bbb2a6cb7b62fd541ba60b4d36519db6
Karen Flower <hello@karenflower.co>
2019-03-08 17:09:47 +0000

    Print list of csv files

--- chartflower.go
+++ chartflower.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"strings"
+)
 
+func main() {
+	csvFilenames := csvFilesinFolder()
+	for _, csvFilename := range csvFilenames {
+		println(csvFilename)
+	}
+}
+
+func csvFilesinFolder() []string {
+	var csvFiles []string
+	filesInFolder, _ := ioutil.ReadDir("./csv/")
+	for _, fileInFolder := range filesInFolder {
+		if strings.HasSuffix(fileInFolder.Name(), "csv") {
+			csvFiles = append(csvFiles, fileInFolder.Name())
+		}
+	}
+	return csvFiles
 }

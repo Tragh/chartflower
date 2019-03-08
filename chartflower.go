225db322e3ee1ba115fbc7c1567a938048e0b5f1
Karen Flower <hello@karenflower.co>
2019-03-08 17:41:35 +0000

    Choose multiple CSVs

--- chartflower.go
+++ chartflower.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"runtime"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	csvFilenames := csvFilesinFolder()
-	for _, csvFilename := range csvFilenames {
-		println(csvFilename)
+	chooseCsvFiles()
+}
+
+func chooseCsvFiles() {
+	csvFiles := getCsvFilesinFolder()
+	println()
+	for index, csvFiles := range csvFiles {
+		csvTrimmed := strings.TrimSuffix(csvFiles, ".csv")
+		fmt.Printf("%v. %v\n", index, csvTrimmed)
+	}
+	print("\nChoose CSVs:")
+	input := getConsoleText()
+	choices := strings.Fields(input)
+	var chosenCsvFiles []string
+
+	for index, csvFile := range csvFiles {
+		csvTrimmed := strings.TrimSuffix(csvFile, ".csv")
+		for _, choice := range choices {
+			if choice == csvFile {
+				chosenCsvFiles = append(chosenCsvFiles, csvFile)
+			} else if choice == csvTrimmed {
+				chosenCsvFiles = append(chosenCsvFiles, csvFile)
+			} else if choice == strconv.Itoa(index) {
+				chosenCsvFiles = append(chosenCsvFiles, csvFile)
+			}
+		}
+	}
+
+	for _, choice := range chosenCsvFiles {
+		println(choice)
 	}
 }
 
-func csvFilesinFolder() []string {
+func getCsvFilesinFolder() []string {
 	var csvFiles []string
 	filesInFolder, _ := ioutil.ReadDir("./csv/")
 	for _, fileInFolder := range filesInFolder {
@@ -21,4 +53,29 @@ func csvFilesinFolder() []string {
 		}
 	}
 	return csvFiles
+}
+
+func getConsoleText() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = removeNewLine(text)
+	typeQToQuit(text)
+	return text
+}
+
+func removeNewLine(input string) string {
+	switch os := runtime.GOOS; os {
+	case "windows":
+		newLineRemoved := strings.TrimSuffix(input, "\r\n")
+		return newLineRemoved
+	default:
+		newLineRemoved := strings.TrimSuffix(input, "\n")
+		return newLineRemoved
+	}
+}
+
+func typeQToQuit(input string) {
+	if input == "q" {
+		os.Exit(0)
+	}
 }
